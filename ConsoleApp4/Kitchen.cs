b02a3aaf5d877c99b5194764d985ea419ab5f4a9
Yousef Hani <91964155+yousefhanni@users.noreply.github.com>
2024-06-26 23:03:33 +0300

    Add project files.

---
+++ ConsoleApp4/Kitchen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public  class Kitchen
+    {
+        //Sync
+        public static void MakeTea()
+        {
+            var water = BoilWater();
+
+            Console.WriteLine("take the cup out");
+            Console.WriteLine("put tea in cup");
+            Console.WriteLine("put sugar in cup");
+            Console.WriteLine($"pour{water} in cup");
+        }
+
+        private static string BoilWater() 
+        {
+            Console.WriteLine("Start the kattle");
+
+            Console.WriteLine("Waiting for  the kattle");
+
+            Task.Delay(5000).GetAwaiter().GetResult();
+
+            Console.WriteLine("kattel  finished boiling");
+
+            return "Water";
+
+        }
+
+        //Async
+        public static async Task MakeTeaAsync()
+        {
+            var boilingwater = BoilWaterAsync();
+
+            Console.WriteLine("take the cup out");
+            Console.WriteLine("put tea in cup");
+            Console.WriteLine("put sugar in cup");
+
+            var water = await boilingwater;
+
+            Console.WriteLine($"pour {water} in cup");
+        }
+
+        private static async Task<string> BoilWaterAsync()
+        {
+            Console.WriteLine("Start the kattle");
+
+            Console.WriteLine("Waiting for  the kattle");
+
+           await Task.Delay(5000);
+
+            Console.WriteLine("kattel  finished boiling");
+
+            return "Water";
+
+        }
+
+    }
+}
